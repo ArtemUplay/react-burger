ed803e3e40f1d92b984e3f60d1f03c1889f0757c
Artem Pronin <artemalexp@gmail.com>
2023-02-23 14:07:39 +0300

    Создана шапка страницы

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import styles from './app.module.css';
+import AppHeader from '../app-header/app-header';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,32 @@
+import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
+import { BurgerIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { ListIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from './app-header.module.css';
+
+const AppHeader = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <ul className={styles.list}>
+          <li className={styles.list__item}>
+            <a href="#" className={`pl-2 pr-5 pb-4 pt-4 mr-2 ${styles.link}`}>
+              <BurgerIcon type="primary" />
+              <span className="ml-2">Конструктор</span></a>
+          </li>
+          <li className="list__item">
+            <a href="#" className={`pl-5 pr-5 pb-4 pt-4 ${styles.link}`}>
+              <ListIcon type="primary" />
+              <span className="ml-2">Лента заказов</span></a>
+          </li>
+        </ul>
+        <Logo />
+        <a href="#" className={`pl-5 pr-4 pb-4 pt-5 ${styles.link} ${styles['personal-account']}`}>
+          <ProfileIcon type="primary" />
+          <span className="ml-2">Личный кабинет</span></a>
+      </nav>
+    </header>
+  );
+}
+
+export default AppHeader;
