30701291bfefc124479d24b60e919ff03d9667a1
Artem Pronin <artemalexp@gmail.com>
2023-05-24 10:18:52 +0300

    Добавлен Bearer к токену при оформлении заказа

--- src/services/actions/order-details.js
+++ src/services/actions/order-details.js
@@ -17,7 +17,7 @@ export function getOrderNumber(burgerConstructorIngredients, accessToken) {
       method: 'POST',
       headers: {
         'Content-type': 'application/json',
-        Authorization: accessToken,
+        Authorization: 'Bearer' + accessToken,
       },
       body: JSON.stringify({
         ingredients: [
